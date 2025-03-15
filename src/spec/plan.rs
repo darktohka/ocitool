f8fa8d917b86b1bbb21b9eb51dcce7117b43cb0f
Disyer <daniel@tohka.us>
2025-03-16 00:43:53 +0200

    spec: Use registry-1 for uploading

--- src/spec/plan.rs
+++ src/spec/plan.rs
@@ -21,9 +21,9 @@ impl ImagePlan {
         let parts: Vec<&str> = self.name.split('/').collect();
 
         if parts.len() > 2 {
-            format!("https://{}", parts[0].to_string())
+            format!("https://{}", parts[0])
         } else {
-            "https://registry.docker.io".to_string()
+            "https://registry-1.docker.io".to_string()
         }
     }
 
